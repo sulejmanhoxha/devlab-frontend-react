cc09d65ab24674c0a707af9b125798379b004462
Filip Nenezic <terreqrue.n.nenezic@gmail.com>
2024-07-26 12:07:41 +0200

    commit 13

--- src/pages/PetsPage.jsx
+++ src/pages/PetsPage.jsx
@@ -1,7 +1,31 @@
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
 import { Card } from "../components/Card";
+import "../css/Listbox.css";
 import { usePets } from "../hooks/usePets";
 
+const people = [
+  { id: 1, name: "Durward Reynolds" },
+  { id: 2, name: "Kenton Towne" },
+  { id: 3, name: "Therese Wunsch" },
+  { id: 4, name: "Benedict Kessler" },
+  { id: 5, name: "Katelyn Rohan" },
+];
+
 const PetsPage = () => {
+  const [selectedPeople, setSelectedPeople] = useState([people[0], people[1]]);
+
+  const handleRemovePerson = (person) => {
+    setSelectedPeople(selectedPeople.filter((p) => p.id !== person.id));
+  };
+
   const { petsQuery } = usePets();
 
   if (petsQuery.isLoading) {
@@ -16,38 +40,31 @@ const PetsPage = () => {
     return (
       <div className="container mx-auto bg-white px-6 py-40">
         <div className="flex w-full gap-4">
-          <div className="w-full max-w-60">
-            <h1 className="text-lg font-semibold tracking-wide">Filters</h1>
-            <div className="space-y-3">
-              <div>
-                <h2>Breed</h2>
-                <p className="rounded-md border border-violet-500 bg-slate-200 px-2 py-3 font-bold">
-                  Any
-                </p>
-              </div>
-
-              <div>
-                <h2>Age</h2>
-                <p className="rounded-md border border-violet-500 bg-slate-200 px-2 py-3 font-bold">
-                  Any
-                </p>
-              </div>
-
-              <div>
-                <h2>Size</h2>
-                <p className="rounded-md border border-violet-500 bg-slate-200 px-2 py-3 font-bold">
-                  Any
-                </p>
-              </div>
-
-              <div>
-                <h2>Gender</h2>
-                <p className="rounded-md border border-violet-500 bg-slate-200 px-2 py-3 font-bold">
-                  Any
-                </p>
-              </div>
-            </div>
-          </div>
+          <Listbox value={selectedPeople} onChange={setSelectedPeople} multiple>
+            <ListboxButton className="listbox-selected">
+              {selectedPeople.map((person) => person.name).join(", ")}
+            </ListboxButton>
+            <ListboxOptions anchor="bottom">
+              {people.map((person) => (
+                <ListboxOption
+                  key={person.id}
+                  value={person}
+                  className="listbox-option"
+                >
+                  <span className="listbox-option-text">{person.name}</span>
+                  {selectedPeople.some((p) => p.id === person.id) && (
+                    <div className="listbox-option-icons">
+                      <CheckIcon className="listbox-option-check-icon" />
+                      <XMarkIcon
+                        className="listbox-option-x-icon"
+                        onClick={() => handleRemovePerson(person)}
+                      />
+                    </div>
+                  )}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Listbox>
 
           <div className="grid flex-1 grid-cols-2 gap-4">
             {petsQuery.data.map((pet) => (
